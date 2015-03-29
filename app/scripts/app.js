e989f7cb4f97014fc59f27ad8378bd7ef4d5e84c
Carlos Velasquez <cv.velasquez@gmail.com>
2015-03-28 22:48:58 -0700

    HTML-CSS

---
+++ app/scripts/app.js
@@ -0,0 +1,4 @@
+bloctime = angular.module('BlocJams', ['ui.router']);
+//angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+    //console.log("Landing.contoller");
+//}]);
